ae4a48010ede7fb149dd2bec43a5a6a844616261
Eitan Seri-Levi <eserilev@ucsc.edu>
2024-10-21 15:45:09 -0700

    minimual example

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+// use clap::builder::styling::AnsiColor;
+// use clap::builder::Styles;
+// use clap::{Parser, ValueEnum};
+use clap::Parser;
+use clap::{builder::ArgPredicate, command, Arg, ArgAction, Command};
+use clap::Subcommand;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Parser, Debug, Serialize, Deserialize)]
+pub struct TestDerive {
+    /// Some option that can be provided via CLI or TOML
+    #[clap(long)]
+    pub some_option: Option<String>,
+
+    /// Path to the TOML configuration file
+    #[clap(long)]
+    pub config_file: Option<String>,
+}
+
+
+fn main() {
+    let cli = TestDerive::parse();
+    let cli = if let Some(config_file) = cli.config_file {
+        let config_content = fs::read_to_string(&config_file).expect("Failed to read config file");
+        toml::from_str(&config_content).expect("Failed to parse TOML")
+    } else {
+        cli
+    };
+    
+
+    // Use the CLI value or fallback to the TOML config value
+    let option_value = cli.some_option;
+    println!("{:?}", option_value);
+}
